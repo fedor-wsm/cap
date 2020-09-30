9551c508bbb6b1233616201641746d8f376d86cd
Fedor <fedor@wsmintl.com>
2020-09-30 09:47:33 -0400

    deleting files

--- .scripts-appspec/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "***Removing Old Release"
-/bin/rm -rf /var/www/staging.luminode.io
-
-echo "***Creating WWWROOT directory"
-/bin/mkdir /var/www/staging.luminode.io
-
-echo "***Cleanup stage is complete!"
